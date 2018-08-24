3ea1c6f61b40de7bf2bbbad1ebf28fb43fac44b1
B533P-04\533 <test@hotmail.com>
2018-08-24 16:50:48 -0400

    Exercice

---
+++ Assets/Script/Rocket.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+    public Rigidbody RocketRigidBody;
+    [SerializeField] public float ForceUpward = 20;
+    [SerializeField] public float ForceSide = 20;
+    // Use this for initialization
+    void Start () {
+        RocketRigidBody = GetComponent<Rigidbody>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetAxis("Jump") > 0)
+        {
+            RocketRigidBody.AddRelativeForce(new Vector3(0, ForceUpward, 0));
+        }
+        if (Input.GetAxis("Horizontal") > 0)
+        {
+            RocketRigidBody.AddForceAtPosition(new Vector3(-ForceSide, 0, 0), new Vector3(0, 5, 0));
+        } else if (Input.GetAxis("Horizontal") < 0)
+        {
+            RocketRigidBody.AddForceAtPosition(new Vector3(ForceSide, 0, 0), new Vector3(0, 5, 0));
+        }
+
+    }
+}
